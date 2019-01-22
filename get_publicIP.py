b16790375f4fdb187bfb31866755ea6994c8aa50
Martynas J <f5AFfMhv@protonmail.com>
2019-01-22 23:06:31 +0200

    My conky template

---
+++ get_publicIP.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python
+
+# Opens file which conatains public IP info from ipinfo.io
+# and process text for conky
+#
+# by Martynas J.
+
+import sys
+
+def update_label(info):
+        try:
+            with open("/tmp/publicIP.tmp","r") as IPfile: 
+                Text = IPfile.read()
+            splited_string_array = Text.split()
+
+            filtered_string_array=[]
+            for i in splited_string_array:
+                filtered_string_array.append(i.replace(':', "").replace(',', "").replace('{', "")
+                    .replace('}', "").replace('"', "").replace(' ', ""))
+
+            filtered_string_array.pop(0)
+            filtered_string_array.pop(-1)
+
+            IP = ""
+            COUNTRY = ""
+            CITY = ""
+            ORG = ""
+
+            count = 0
+            for j in filtered_string_array:
+                if j == "ip":
+                    IP = filtered_string_array[count+1]
+                elif j == "city":
+                    CITY = filtered_string_array[count+1]
+                elif j == "country":
+                    COUNTRY = filtered_string_array[count+1]
+                elif j == "org":
+                    ORG = " ".join(filtered_string_array[count+1:-1])
+
+                count = count + 1
+            
+            if info == "IP":
+                print IP
+            elif info == "COUNTRY":
+                print COUNTRY
+            elif info == "CITY":
+                print CITY
+            elif info == "ORG":
+                print ORG
+            else:
+                print "'" + info + "'" + " is invalid parameter. Only use: IP, CITY, COUNTRY, ORG"
+
+        except:
+            print "Error: /tmp/publicIP.tmp doesn't exist."
+
+
+def main():
+    try:
+        update_label(sys.argv[1])
+    except:
+        print "Error. Pass only one of these parameters to function: IP, CITY, COUNTRY, ORG"
+
+main()
+     
