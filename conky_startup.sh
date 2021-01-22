dba6e94e291ab11ebdc0553927049be78a14f4dd
f5AFfMhv <f5AFfMhv@protonmail.com>
2021-01-22 20:53:23 +0200

    first commit

--- conky_startup.sh
+++ conky_startup.sh
@@ -4,4 +4,4 @@
 sleep 10
 /usr/bin/conky -q -c ~/.config/conky/conkyrc
 /usr/bin/conky -q -c ~/.config/conky/conkyrc2
-/usr/bin/conky -q -c ~/.config/conky/conkyrc3
+#/usr/bin/conky -q -c ~/.config/conky/conkyrc3
